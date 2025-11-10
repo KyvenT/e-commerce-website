68b58534c06de2da839e2dfb1de3ece4f57128d4
KyvenT <kyventran@gmail.com>
2025-11-09 22:53:21 -0500

    fix mobile sizing, styling

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,6 +2,13 @@ import { Eye, EyeClosed } from "lucide-react";
 import { Button } from "../ui/button";
 import { useRef, useState } from "react";
 import { Spinner } from "../ui/shadcn-io/spinner";
+import { useNavigate } from "react-router";
+
+/*
+ * sample FakeStoreAPI user credentials
+ * username: donero
+ * password: ewedon
+ */
 
 export const Login = () => {
   const [isShowingPassword, setIsShowingPassword] = useState<boolean>(false);
@@ -9,6 +16,7 @@ export const Login = () => {
   const [loginLoading, setLoginLoading] = useState<boolean>(false);
   const usernameInputRef = useRef<HTMLInputElement>(null);
   const passwordInputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   const handleShowPasswordClick = () => {
     setIsShowingPassword((prev) => !prev);
@@ -32,6 +40,8 @@ export const Login = () => {
       }
       const result = await data.json();
       console.log(result.token);
+      // store token in context/cookie later
+      navigate("/store");
     } catch (err) {
       if (err instanceof Error) setLoginError(err.message);
     } finally {
@@ -41,27 +51,27 @@ export const Login = () => {
 
   return (
     <div className="h-full flex justify-center items-center">
-      <div className="border w-[25%] h-fit py-[16px] flex flex-col items-center gap-[16px]">
-        <h1 className="text-2xl py-[20px]">Login</h1>
+      <div className="border w-[80%] md:w-[50%] lg:w-[25%] p-[16px] flex flex-col items-center gap-[16px]">
+        <h1 className="text-3xl select-none py-[20px]">Login</h1>
         <form
           onSubmit={handleLogin}
-          className="flex flex-col items-center gap-[16px]"
+          className="w-full flex flex-col items-center gap-[16px]"
         >
           <input
-            className="w-[80%] border border-gray-400 rounded-xs px-2 py-1 placeholder:text-gray-500"
+            className="w-[80%] text-base border border-gray-400 rounded-xs px-2 py-1 placeholder:text-gray-500 outline-none"
             type="text"
             placeholder="Username..."
             ref={usernameInputRef}
           />
           <div className="w-[80%] flex items-center border border-gray-400 rounded-xs px-2 py-1">
             <input
-              className="flex-1 placeholder:text-gray-500 p-[0] outline-none"
+              className="flex-1 text-base min-w-0 placeholder:text-gray-500 p-[0] outline-none"
               {...(isShowingPassword ? { type: "text" } : { type: "password" })}
               placeholder="Password..."
               ref={passwordInputRef}
             />
             <Button
-              className="w-fit h-fit cursor-pointer hover:bg-transparent hover:text-gray-500 active:text-gray-200 bg-transparent text-black"
+              className="w-[1rem] h-[1rem] cursor-pointer hover:bg-transparent hover:text-gray-500 active:text-gray-200 bg-transparent text-black"
               onClick={handleShowPasswordClick}
               type="button"
             >
@@ -72,7 +82,10 @@ export const Login = () => {
               )}
             </Button>
           </div>
-          <Button type="submit" className="cursor-pointer active:bg-gray-600">
+          <Button
+            type="submit"
+            className="cursor-pointer text-lg active:bg-gray-600"
+          >
             Login
           </Button>
         </form>
