f8ff547c73eb66e48645a050c55e7cbef81a5de6
KyvenT <kyventran@gmail.com>
2025-11-23 23:02:31 -0500

    add mobile responsiveness to navbar

--- src/components/Landing/layout/LandingNav.tsx
+++ src/components/Landing/layout/LandingNav.tsx
@@ -1,7 +1,9 @@
-import { Cat, LogIn } from "lucide-react";
+import { Cat, LogIn, Menu } from "lucide-react";
 import { Button } from "../../ui/button";
 import { LandingNavLink } from "./LandingNavLink";
 import { NavLink } from "react-router";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { useState } from "react";
 
 type DisplayURL = {
   display: string;
@@ -15,28 +17,47 @@ const linkURLs: DisplayURL[] = [
 ];
 
 export const LandingNav = () => {
+  const isMobile = useIsMobile();
+  const [displayMobileNav, setDisplayMobileNav] = useState<boolean>(false);
+
+  const handleMobileNavBtnClick = () => {
+    setDisplayMobileNav((prev) => !prev);
+  };
+
   return (
-    <nav className="sticky top-0 h-[15%] md:h-[10%] flex justify-between items-center px-4 py-4 bg-white border border-black text-nowrap">
+    <nav className="sticky top-0 h-[15%] md:h-[10%] w-[100dvw] flex justify-between items-center px-4 py-4 bg-white border border-black text-nowrap">
       <div className="flex items-center gap-1 select-none">
         <Cat className="text-white bg-black" />
         <h1 className="text-lg">Cat Brand</h1>
       </div>
-      <ul className="w-[50%] flex items-center justify-evenly wrap-normal">
-        {linkURLs.map((link) => (
-          <li>
-            <h2 className="hover:underline decoration-1">
-              <LandingNavLink to={link.url}>{link.display}</LandingNavLink>
-            </h2>
-          </li>
-        ))}
-      </ul>
-      <div>
-        <Button>
-          <NavLink className="flex items-center gap-1" to="/login">
-            Log In <LogIn />
-          </NavLink>
+      {isMobile && (
+        <Button onClick={handleMobileNavBtnClick}>
+          <Menu />
         </Button>
-      </div>
+      )}
+      {(!isMobile || displayMobileNav) && (
+        <div className="bg-white gap-[8px] w-[80%] md:w-[100%] border absolute md:[static] top-0 right-0 md:static flex flex-col md:flex-row items-center">
+          <ul className="w-full flex-1 flex flex-col md:flex-row items-center justify-evenly wrap-normal">
+            {linkURLs.map((link) => (
+              <li key={link.url}>
+                <h2 className="hover:underline decoration-1">
+                  <LandingNavLink to={link.url}>{link.display}</LandingNavLink>
+                </h2>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <Button>
+              <NavLink className="flex items-center gap-1" to="/login">
+                Log In <LogIn />
+              </NavLink>
+            </Button>
+          </div>
+          <Button onClick={handleMobileNavBtnClick}>
+            <Menu />
+          </Button>
+        </div>
+      )}
     </nav>
   );
 };
