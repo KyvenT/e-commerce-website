f0edb5957f19ca949cb571e458c8662912bbc72f
Dylan <dylantrojek@outlook.com>
2026-01-18 14:04:49 -0500

    set up initial layout on product page

--- src/components/Product/ProductPage.tsx
+++ src/components/Product/ProductPage.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Spinner } from "../ui/shadcn-io/spinner";
-import { LandingNav } from "../Landing/layout/LandingNav";
 
 type Product = {
   id: number;
@@ -51,13 +50,14 @@ export const ProductPage = () => {
   return (
     <div className="h-full relative">
       {/* breadcrumbs here, but for now just the h3*/ }
-      <h3>{productData?.category}</h3>
-      <h1>
-        Product {productId} {productData?.title}
-      </h1>
-      <img src={productData?.image} className="left-0 top-0"></img>
-      <p>{productData?.description}</p>
-      <h2>{productData?.price}</h2>
+      <h3 className="text-neutral-800 dark:text-neutral-300 text-sm">{productData?.category}</h3>
+       {/* product card, for now it will be 1 column with vertical scroll, but on larger screen sizes switch to a 2 column grid */} 
+        <section className="grid grid-cols-1 md:grid-cols-2">
+          <h1 className="text-3xl">Product {productId} {productData?.title}</h1>
+          <h2 className="text-xl">{productData?.price}</h2>
+          <img src={productData?.image} className="left-0 top-0"></img>
+          <p className="text-neutral-800 text-sm">{productData?.description}</p>
+        </section>
     </div>
   );
 };
