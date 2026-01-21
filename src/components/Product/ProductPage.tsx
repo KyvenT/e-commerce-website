2999c8b436d68986eed444e0febb0964aed14009
Dylan <dylantrojek@outlook.com>
2026-01-21 15:05:35 -0500

    rewrote ui code for product page

--- src/components/Product/ProductPage.tsx
+++ src/components/Product/ProductPage.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { NavLink } from "react-router";
 import { Spinner } from "../ui/shadcn-io/spinner";
+import { Button } from "../ui/button";
 
 type Product = {
   id: number;
@@ -47,17 +49,42 @@ export const ProductPage = () => {
 
   if (isLoading) return <Spinner />;
 
+
   return (
     <div className="h-full relative">
-      {/* breadcrumbs here, but for now just the h3*/ }
-      <h3 className="text-neutral-800 dark:text-neutral-300 text-sm">{productData?.category}</h3>
-       {/* product card, for now it will be 1 column with vertical scroll, but on larger screen sizes switch to a 2 column grid */} 
-        <section className="grid grid-cols-1 md:grid-cols-2">
-          <h1 className="text-3xl">Product {productId} {productData?.title}</h1>
-          <h2 className="text-xl">{productData?.price}</h2>
-          <img src={productData?.image} className="left-0 top-0"></img>
-          <p className="text-neutral-800 text-sm">{productData?.description}</p>
+      {/* breadcrumbs here, for now just the h3*/ }
+      <h3 className="text-neutral-800 dark:text-neutral-300 text-sm">{productData?.category}</h3> 
+        <section className="relative grid md:grid-cols-2 md:grid-rows-1 p-5 md:p-10">
+          <img src={productData?.image} className="min-w-1/5 max-w-2/5 justify-self-center"></img>
+          {/* product information container */}
+          <div className="relative **:py-0.5">
+            <h1 className="text-3xl">Product {productId} {productData?.title}</h1>
+            {/* in theory, don't use the dollar sign and actually get a preferred currency, but for now this is what i'm doing */}
+            <h2 className="text-2xl">{"$" + productData?.price}</h2>
+            <p className="text-neutral-800 text-base">{productData?.description}</p>
+            {/* button container*/}
+            <div className="grid place-content-evenly md:place-content-start gap-y-2 my-2"> 
+              <Button variant="outline">
+                <NavLink
+                className="flex items-center gap-1 text-base font-normal"
+                to="/cart"
+                >
+                Add to cart
+                </NavLink>
+              </Button>
+              <Button variant="outline" className="bg-teal-500 hover:bg-teal-700 hover:text-neutral-50 text-neutral-50">
+                <NavLink
+                  className="flex items-center gap-1 text-base font-normal"
+                  to="/login"
+                >
+                Buy now
+                </NavLink>
+              </Button>
+            </div> 
+          </div>
         </section>
     </div>
   );
 };
+
+
